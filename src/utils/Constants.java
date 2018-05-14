9ab552d959507df718e0687ea5072f14220d27a4
gdumral <35218482+gdumral@users.noreply.github.com>
2018-05-14 15:49:02 +0200

    capture http error codes

---
+++ src/utils/Constants.java
@@ -0,0 +1,21 @@
+package utils;
+
+public  interface Constants {
+	
+	public final String BASE_URL = "https://www.ziggogo.tv/nl.html";
+	public final String HAR_FILE_LABEL = "ziggo.nl";
+	public final String HAR_FILE_PATH = "C:\\\\TestAutomation\\\\NetworkTraffic\\\\ziggo.har";
+	public final String DRIVER_PATH = "C:\\TestAutomation\\chromedriver.exe";
+	public static final String USER_NAME = "lgtest";
+	public static final String PASSWORD = "S0ftware";
+	public static final String HTTP_400_ERROR = "HTTP 400 Bad Request";
+	public static final String HTTP_401_ERROR = "HTTP 401 Unauthorized";
+	public static final String HTTP_403_ERROR = "HTTP 403 Forbidden";
+	public static final String HTTP_404_ERROR = "HTTP 404 Not Found";
+	public static final String HTTP_500_ERROR = "HTTP 500 Internal Server Error";
+	public static final String HTTP_502_ERROR = "HTTP 502 Bad Gateway";
+	public static final String HTTP_503_ERROR = "HTTP 503 Service Unavailable";
+	public static final String HTTP_504_ERROR = "HTTP 504 Gateway Timeout";
+	public static final String CONVIVA_REQUEST = "https://cws.conviva.com";
+
+}
